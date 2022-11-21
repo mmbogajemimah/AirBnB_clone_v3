16145982a1eccc699f98c01efa4a25fccb3cf53d
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-11-21 19:34:46 +0000

    Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,14 +16,14 @@ def status_okay():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
     """ Retrieves the number of each objects by type """
     classes = [Amenity, City, Place, Review, State, User]
     names = ["amenities", "cities", "places", "reviews", "states", "users"]
 
-    number_objects = {}
+    numberof_objects = {}
     for i in range(len(classes)):
-        number_objects[names[i]] = storage.count(classes[i])
+        numberof_objects[names[i]] = storage.count(classes[i])
 
-    return jsonify(number_objects)
+    return jsonify(numberof_objects)
