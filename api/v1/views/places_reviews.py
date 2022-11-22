fc8afc1c3ef92940d900bb893bc1b9bdedcdbd62
Roncollins MM <muriukiron@gmail.com>
2022-11-22 22:35:58 +0300

    Default API handler for Review objects

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,9 +4,6 @@ from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from models.user import User
-from models.amenity import Amenity
-from models.state import State
-from models.city import City
 from models.place import Place
 from models.review import Review
 
